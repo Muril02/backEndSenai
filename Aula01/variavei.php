de2dd295196b923b931144694df4e2683f583ab4
Muril02 <muriloalmeidadeoliveira19@gmail.com>
2025-08-01 20:48:47 -0300

    01/08/25

---
+++ Aula01/variavei.php
@@ -0,0 +1,31 @@
+<?php
+    
+    $nome = "Manoel gomis \n";
+    $idade = '54';
+    $ano_atual  = 2025;
+
+    $data_nasc = $ano_atual-$idade;
+    echo $nome, "você nasceu em: ", $data_nasc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
